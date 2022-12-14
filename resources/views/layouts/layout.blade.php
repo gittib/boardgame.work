e2c2569d3ac98ccbe01037780bef4b37259fff01
gitti <gitti.akihisa.ad@gmail.com>
2022-12-14 11:50:06 +0900

    mix周り修正

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -27,6 +27,8 @@
         </main>
 
         <script src="https://code.jquery.com/jquery-3.6.1.slim.min.js" integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA=" crossorigin="anonymous"></script>
+        <script src="{{ mix('/js/manifest.js') }}"></script>
+        <script src="{{ mix('/js/vendor.js') }}"></script>
         <script src="{{ mix('/js/app.js') }}"></script>
         @yield('additional_scripts')
     </body>
