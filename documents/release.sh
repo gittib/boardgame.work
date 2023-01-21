43d30476bb47c248d6c771ee467302e4d2c90cc4
gitti <gitti.akihisa.ad@gmail.com>
2023-01-21 17:46:39 +0900

    リリーススクリプト微修正

--- documents/release.sh
+++ documents/release.sh
@@ -4,11 +4,13 @@ git pull
 git checkout master
 git merge develop -m "Merge branch 'develop'"
 git push
+composer install
+npm ci
 npm run prod
 php artisan view:cache
 php artisan config:cache
 
 
 ## ワンライナー版
-git checkout -f develop && git pull && git checkout master && git merge develop -m "Merge branch 'develop'" && git push && npm run prod && php artisan view:cache && php artisan config:cache
+git checkout -f develop && git pull && git checkout master && git merge develop -m "Merge branch 'develop'" && git push && composer install && npm ci && npm run prod && php artisan view:cache && php artisan config:cache
 
