591e950c8604f6857ebfc6d1c347d9e5ee976da7
gitti <gitti.akihisa.ad@gmail.com>
2023-01-27 10:53:27 +0900

    英語版公式サイトのリンクとか修正

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -8,7 +8,9 @@ return [
         ],
     ],
 
-    'what_is_this' => 'This is a website where you can share your Tragedy Looper Scripts. Anyone can read the Script. If you have a Twitter account, you can also create and publish Scripts.',
+    'official_url' => 'http://bakafire.main.jp/game01.html',
+
+    'what_is_this' => 'This is a website where you can share your :trlink Scripts. Anyone can read the Script. If you have a Twitter account, you can also create and publish Scripts.',
     'footer' => 'This is a fan site for BakaFire Party\'s game ":trlink". If you have any questions about this site, please contact :manager.',
     'loop_day_summary' => '<span class="loop">:loops</span> loops <span class="day">:days</span> days',
     'special_rule_placeholder' => 'If this Script corresponds to plus, please indicate so in the special rules.',
--- resources/lang/ja/messages.php
+++ resources/lang/ja/messages.php
@@ -8,7 +8,9 @@ return [
         ],
     ],
 
-    'what_is_this' => 'こちらは、<span>惨劇RoopeRの脚本を</span><span>共有できるWebサイトです。</span>どなたでも脚本を閲覧でき、またTwitterアカウントがあれば<span>自由に脚本を作成・公開することができます。</span>',
+    'official_url' => 'http://bakafire.main.jp/rooper/sr_top.htm',
+
+    'what_is_this' => 'こちらは、<span>:trlinkの脚本を</span><span>共有できるWebサイトです。</span>どなたでも脚本を閲覧でき、またTwitterアカウントがあれば<span>自由に脚本を作成・公開することができます。</span>',
     'footer' => '本サイトはBakaFire Partyのゲーム「:trlink」のファンサイトです。本サイトについてのお問い合わせは、:managerまでご連絡ください。',
     'loop_day_summary' => '<span class="loop">:loops</span>ループ<span class="day">:days</span>日',
     'special_rule_placeholder' => '＋対応の場合、特殊ルールにその旨を記載してください',
