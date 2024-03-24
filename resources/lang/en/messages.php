e993ce57a0b58ebcbe708a4bfef454e15b35bdb9
gitti <gitti.akihisa.ad@gmail.com>
2024-03-24 10:06:36 +0900

    is_plusフラグを追加

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -13,7 +13,6 @@ return [
     'what_is_this' => 'This is a website where you can share your :trlink Scripts. Anyone can read the Script. If you have a Twitter account, you can also create and publish Scripts.',
     'footer' => 'This is a fan site for BakaFire Party\'s game ":trlink". If you have any questions about this site, please contact :manager.',
     'loop_day_summary' => '<span class="loop">:loops</span> loops <span class="day">:days</span> days',
-    'special_rule_placeholder' => 'If this Script corresponds to plus, please indicate so in the special rules.',
     'invalid_conditions' => 'This Script seems to cast the roles incorrectly.<br>It may be normal depending on the special rules, so please check it before using.',
 
     'what_is_advice_quiz' => 'Quiz for the Mastermind is quiz game that guesses the Hints for the Mastermind after checking the Open card and the Secret card.',
