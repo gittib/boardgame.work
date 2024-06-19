fb6e36e3e56a7449e3f65672fd32daf77dda9665
gitti <gitti.akihisa.ad@gmail.com>
2024-06-19 22:23:45 +0900

    特殊ルールの文字数上限とか設定

--- app/Http/Requests/PostScenario.php
+++ app/Http/Requests/PostScenario.php
@@ -25,6 +25,7 @@ class PostScenario extends FormRequest
         return [
             'title' => 'required_with:is_open',
             'rule_x2_id' => 'different:rule_x1_id',
+            'special_rule' => 'nullable|string|max:200',
             'scenario_chara.*.character_id' => 'distinct',
             'feature' => 'required_with:is_open',
             'advice' => 'required_with:is_open',
@@ -43,6 +44,7 @@ class PostScenario extends FormRequest
     public function attributes() {
         return [
             'title' => __('脚本タイトル'),
+            'special_rule' => __('特殊ルール'),
             'scenario_chara.*.character_id' => __('登場人物'),
             'feature' => __('脚本の特徴'),
             'advice' => __('脚本家への指針'),
