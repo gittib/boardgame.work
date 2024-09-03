9b6443488305bba1184dea29a52eaf588af6d78b
gitti <gitti.akihisa.ad@gmail.com>
2024-09-03 19:44:06 +0900

    テストコード書いてみる

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,18 @@
 
 namespace Tests\Feature;
 
+use App\Http\Middleware\DevBasicAuth;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutMiddleware(DevBasicAuth::class);
+    }
+
     /**
      * A basic test example.
      *
