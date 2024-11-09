538daecc08f1f4d6b2b005914dda21b6252a3296
gitti <gitti.akihisa.ad@gmail.com>
2024-11-09 17:47:47 +0900

    リリース手順書修正

--- documents/release.sh
+++ documents/release.sh
@@ -1,17 +1,13 @@
 ## 本番リリース用コマンド
-git checkout -f develop
-git pull
-git checkout master
-git merge develop -m "Merge branch 'develop'"
-git push
-composer install
-npm ci
-npm run prod
-php artisan view:cache
-php artisan config:cache
+git checkout -f develop && \
+git pull && \
+git checkout master && \
+git merge develop -m "Merge branch 'develop'" && \
+git push && \
+composer install && \
+npm ci && \
+npm run prod && \
+php artisan view:cache && \
+php artisan config:cache && \
 php artisan route:cache
-
-
-## ワンライナー版
-git checkout -f develop && git pull && git checkout master && git merge develop -m "Merge branch 'develop'" && git push && composer install && npm ci && npm run prod && php artisan view:cache && php artisan config:cache && php artisan route:cache
 
